89ab7c3df379a1fdf141679031bb1a2b1a7b9409
ngoeminne <nicogoeminne@gmail.com>
2021-02-18 20:59:09 +0100

    - updated web configs for IIS hosting

--- Arcmage.Web/src/arcmage/Game/config.prod.js
+++ Arcmage.Web/src/arcmage/Game/config.prod.js
@@ -1,3 +1,3 @@
-const gameApiUri = "https://aminduna.arcmage.org:9090";
-const apiUri = "https://aminduna.arcmage.org";
-const portalUri = "https://aminduna.arcmage.org";
+const gameApiUri = "https://aminduna.arcmage.org:5090";
+const apiUri = "https://aminduna.arcmage.org:5000";
+const portalUri = "https://aminduna.arcmage.org:5000";
