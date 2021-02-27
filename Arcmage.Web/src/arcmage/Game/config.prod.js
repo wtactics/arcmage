30fb79c2298103cbe5151226b0ad419a9c15f839
ngoeminne <nicogoeminne@gmail.com>
2021-02-27 12:47:52 +0100

    - added ssh updater for the game runtime (this runs on a ssl cert renewal after aminduna.arcmage.org:5000 has been updated, we're using the same cert for the game runtime)

--- Arcmage.Web/src/arcmage/Game/config.prod.js
+++ Arcmage.Web/src/arcmage/Game/config.prod.js
@@ -1,3 +1,3 @@
-const gameApiUri = "http://aminduna.arcmage.org:5090";
-const apiUri = "http://aminduna.arcmage.org:5000";
-const portalUri = "http://aminduna.arcmage.org:5000";
+const gameApiUri = "https://aminduna.arcmage.org:5090";
+const apiUri = "https://aminduna.arcmage.org:5000";
+const portalUri = "https://aminduna.arcmage.org:5000";
